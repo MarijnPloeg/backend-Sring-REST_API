6c7ebcdbfa9178a7eb5487ed1be8397fd65664b9
Marijn Ploeg <marijnploeg@gmail.com>
2021-04-20 13:47:25 +0200

    initialized Spring Boot

---
+++ src/main/java/nl/marijnploeg/kitereparatie/KitereparatieApplication.java
@@ -0,0 +1,13 @@
+package nl.marijnploeg.kitereparatie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitereparatieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitereparatieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/marijnploeg/kitereparatie/KitereparatieApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.marijnploeg.kitereparatie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitereparatieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
